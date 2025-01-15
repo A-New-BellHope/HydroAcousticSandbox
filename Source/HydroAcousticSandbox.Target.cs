ba863757e433701f2f4b11753e81a04a5f4269a9
Khalil Jackson <kjaxon6@gmail.com>
2025-01-14 18:03:53 -0800

    init

--- Source/HydroAcousticSandbox.Target.cs
+++ Source/HydroAcousticSandbox.Target.cs
@@ -8,8 +8,8 @@ public class HydroAcousticSandboxTarget : TargetRules
 	public HydroAcousticSandboxTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		DefaultBuildSettings = BuildSettingsVersion.V4;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
 		ExtraModuleNames.Add("HydroAcousticSandbox");
 	}
 }
