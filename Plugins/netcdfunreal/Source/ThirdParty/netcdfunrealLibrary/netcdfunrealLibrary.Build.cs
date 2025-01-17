460cc2778601e248fac47e11cf6e38251a9ac730
Khalil Jackson <kjaxon6@gmail.com>
2025-01-17 12:36:22 -0800

    init

---
+++ Plugins/netcdfunreal/Source/ThirdParty/netcdfunrealLibrary/netcdfunrealLibrary.Build.cs
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using System.IO;
+using UnrealBuildTool;
+
+public class netcdfunrealLibrary : ModuleRules
+{
+	public netcdfunrealLibrary(ReadOnlyTargetRules Target) : base(Target)
+	{
+		Type = ModuleType.External;
+
+		if (Target.Platform == UnrealTargetPlatform.Win64)
+		{
+			// Add the import library
+			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "bin", "hdf5.lib"));
+			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "bin", "hdf5_hl.lib"));
+			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "bin", "libcurl.lib"));
+			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "bin", "netcdf.lib"));
+			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "bin", "netcdf-cxx4.lib"));
+			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "bin", "pkgconf.lib"));
+			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "bin", "szip.lib"));
+			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "bin", "zlib.lib"));
+
+			// Delay-load the DLL, so we can load it from the right place first
+			PublicDelayLoadDLLs.Add("hdf5.dll");
+			PublicDelayLoadDLLs.Add("hdf5_hl.dll");
+			PublicDelayLoadDLLs.Add("libcurl.dll");
+			PublicDelayLoadDLLs.Add("netcdf.dll");
+			PublicDelayLoadDLLs.Add("netcdf-cxx4.dll");
+			PublicDelayLoadDLLs.Add("pkgconf.dll");
+			PublicDelayLoadDLLs.Add("szip.dll");
+			PublicDelayLoadDLLs.Add("zlib.dll");
+
+			// Ensure that the DLL is staged along with the executable
+			RuntimeDependencies.Add("$(PluginDir)/Binaries/ThirdParty/netcdfunrealLibrary/Win64/hdf5.dll");
+			RuntimeDependencies.Add("$(PluginDir)/Binaries/ThirdParty/netcdfunrealLibrary/Win64/hdf5_hl.dll");
+			RuntimeDependencies.Add("$(PluginDir)/Binaries/ThirdParty/netcdfunrealLibrary/Win64/libcurl.dll");
+			RuntimeDependencies.Add("$(PluginDir)/Binaries/ThirdParty/netcdfunrealLibrary/Win64/netcdf.dll");
+			RuntimeDependencies.Add("$(PluginDir)/Binaries/ThirdParty/netcdfunrealLibrary/Win64/netcdf-cxx4.dll");
+			RuntimeDependencies.Add("$(PluginDir)/Binaries/ThirdParty/netcdfunrealLibrary/Win64/pkgconf.dll");
+			RuntimeDependencies.Add("$(PluginDir)/Binaries/ThirdParty/netcdfunrealLibrary/Win64/szip.dll");
+			RuntimeDependencies.Add("$(PluginDir)/Binaries/ThirdParty/netcdfunrealLibrary/Win64/zlib.dll");
+        }
+        else if (Target.Platform == UnrealTargetPlatform.Mac)
+        {
+        }
+        else if (Target.Platform == UnrealTargetPlatform.Linux)
+		{
+		}
+	}
+}
